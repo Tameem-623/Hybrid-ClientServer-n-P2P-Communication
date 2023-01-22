ebca4135798f54dd1b951ac0403f7f7ef83fd54a
Tameem <79134847+Tameem-623@users.noreply.github.com>
2023-01-22 21:21:20 +0500

    Established P2P Communication
    Established P2P Communication locally on localhost. Now have to try over the LAN and add chatting features as per requirements.

--- CLIENT.c
+++ CLIENT.c
@@ -1,13 +1,166 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
-#define PORT 12345 // Port number to use for the server
+#define MAX_MESSAGE_LENGTH 1024
+#define ENDPOINTPORT 3592
 
-
-int main(int argc, char *argv[])
+void Server_Chat(int socket)
 {
+    char buf[5];
+    read(socket, buf, 5);
+    printf("Message : %s\n", buf);
+    write(socket, "Hi", strlen("Hi") + 1);
+}
+void Client_Chat(int socket)
+{
+    char *message;
+    message = "Hello";
+    write(socket, message, strlen(message) + 1);
+    sleep(1);
+    read(socket, message, strlen("Hi")+1);
+    printf("Message from Client : %s", message);
+}
+int main(int agrc, char *argv[])
+{
+    int netwrok_socket;
+    // int socket(int __domain, int __type, int __protocol) - Returns fd or -1
+
+    netwrok_socket = socket(PF_INET, SOCK_STREAM, 0);
+    // 0 - default protocol
+    // SOCK_STEAM - TCP
+    // AF_INET / PF_INET - IPV4
+
+    if (netwrok_socket == -1)
+    {
+        perror("FAILED to create socket: ");
+        return -1;
+    }
+
+    // specifying end point for client or adress for socket
+    struct sockaddr_in server_address;
+    server_address.sin_family = PF_INET;
+    server_address.sin_port = htons(12345);
+    server_address.sin_addr.s_addr = inet_addr(argv[1]); // Address to accept any incoming messages.
+
+    int connection_status = connect(netwrok_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+    // int connect(int __fd, const struct sockaddr *__addr, socklen_t __len)
+    // Open a connection on socket FD to peer at ADDR (which LEN bytes long).
+    // For connectionless socket types, just set the default address to send to
+    // and the only address from which to accept transmissions.
+    // Return 0 on success, -1 for errors.
+
+    if (connection_status == -1)
+    {
+        perror("FAILED to estabslihed a connection with endpoint : ");
+        return -1;
+    }
+    else
+    {
+        printf("[*] Connected to Indexer Service.\n");
+    }
+
+    char message[MAX_MESSAGE_LENGTH];
+    char server_response[257];
+    int choice = 7;
+    while (1)
+    {
+        if (choice == 7) // to send endpoint to server.
+        {
+            // for now just port since its a localhost
+            write(netwrok_socket, &choice, sizeof(int));
+            sleep(1);
+            int port = ENDPOINTPORT;
+            write(netwrok_socket, &port, sizeof(int));
+        }
+        printf("[*] Press\n\t1. See Online Users\n\t2. Connect to an Endpoint.\n\t3. Exit\n\t-> Enter Choice : ");
+        scanf("%d", &choice);
+ 
+        if (choice == 1)
+        {
+            write(netwrok_socket, &choice, sizeof(int));
+            sleep(1);
+            char LIST[MAX_MESSAGE_LENGTH];
+            read(netwrok_socket, LIST, MAX_MESSAGE_LENGTH);
+            printf("[*] Online Users\n%s", LIST);
+            continue;
+        }
+        else if (choice == 2)
+        {
+            close(netwrok_socket);
+            int port_nmbr;
+            // Send a specific identifier to get relevant address from client
+            printf("[*] Enter PORT Number : ");
+            scanf("%d", &port_nmbr);
+            int P2P_socket;
+            P2P_socket = socket(PF_INET, SOCK_STREAM, 0);
+            struct sockaddr_in P2P_address;
+            P2P_address.sin_family = PF_INET;
+            P2P_address.sin_port = htons(port_nmbr);
+            P2P_address.sin_addr.s_addr = inet_addr(argv[1]);
+
+            // prompt user to act as client or server, this should be mutually exclusive.
+            printf("[*] Act as Server (0) : ");
+            scanf("%d", &port_nmbr);
+            if (port_nmbr == 0) // this is for server
+            {
+                // bind the socket to localhost port
+                if (bind(P2P_socket, (struct sockaddr *)&P2P_address, sizeof(P2P_address)) < 0)
+                {
+                    perror("[-] bind FAILED");
+                    return EXIT_FAILURE;
+                }
+                printf("[*] Listener on port %d \n", ENDPOINTPORT);
+
+                if (listen(P2P_socket, 1) < 0)
+                {
+                    perror("[-] listen FAILED");
+                    exit(EXIT_FAILURE);
+                }
+                int client_socket;
+                if ((client_socket = accept(P2P_socket, NULL, NULL)) < 0)
+                {
+                    perror("[-] accept FAILED");
+                    return EXIT_FAILURE;
+                }
+                // here loop for chatting
+                Server_Chat(client_socket);
+            }
+            else // for acting as client
+            {
+
+                int connection_status_P2P = connect(P2P_socket, (struct sockaddr *)&P2P_address, sizeof(P2P_address));
+                if (connection_status_P2P == -1)
+                {
+                    perror("FAILED to estabslihed a connection with endpoint : ");
+                    return -1;
+                }
+                else
+                {
+                    printf("[*] Ready for P2P Communication.\n");
+                }
+                // here loop for chatting
+                Client_Chat(P2P_socket);
+            }
+            break;
+        }
+        else if (choice == 3)
+        {
+            // close the conncetion to server if any
+            if (connection_status != 0)
+                close(netwrok_socket);
+            // Exit the Client
+            return EXIT_SUCCESS;
+        }
+        else
+        {
+            printf("[-] Invalid Entery!\n");
+        }
+    }
     return 0;
 }
---
+++ G4GServer.c
@@ -0,0 +1,306 @@
+// Example code: A simple server side code, which echos back the received message.
+// Handle multiple socket connections with select and fd_set on Linux
+#include <stdio.h>
+#include <string.h> //strlen
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>    //close
+#include <arpa/inet.h> //close
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
+
+// for IP printing
+#include <netdb.h>
+#include <ifaddrs.h>
+
+#define TRUE 1
+#define FALSE 0
+#define PORT 12345
+#define MAX_CLIENTS 4
+#define BUFFER_SIZE 1024
+#define MAX_MESSAGE_LENGTH 1024
+#define NI_MAXHOST 1025
+#define NI_NUMERICHOST 1
+
+typedef struct
+{
+    char *endpointIP;
+    int endpointPort;
+}ENDPOINT;
+
+typedef struct
+{
+    char *Name;
+    char *IP;
+    int port;
+    int status;
+    ENDPOINT client_EP;
+} CLIENTDB;
+
+void CLIENTDB_ZERO(CLIENTDB *Entry)
+{
+    // Clearning the garbage Value
+    Entry->Name = '\0';
+    Entry->IP = '\0';
+    Entry->port = 0;
+    Entry->status = 0;
+    Entry->client_EP.endpointPort = 0;
+    Entry->client_EP.endpointIP = '\0';
+}
+
+void CLIENTDB_ADD(CLIENTDB *Entry, char *name, char *ip, int port, int Status)
+{
+    Entry->Name = name;
+    Entry->IP = ip;
+    Entry->port = port;
+    Entry->status = Status;
+}
+
+void printIP()
+{
+    struct ifaddrs *ifaddr, *ifa;
+    int family;
+    char host[NI_MAXHOST];
+
+    if (getifaddrs(&ifaddr) == -1)
+    {
+        perror("getifaddrs");
+        return;
+    }
+    // Iterate through the list of interfaces
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)
+    {
+        if (ifa->ifa_addr == NULL)
+        {
+            continue;
+        }
+        family = ifa->ifa_addr->sa_family;
+
+        // Only consider IPv4
+        if (family == AF_INET)
+        {
+            // Get the address as a string
+            getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in), host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+
+            if (!strcmp(ifa->ifa_name, "eth0"))
+            {
+                // printf("Interface: %s\n", ifa->ifa_name);
+                printf("IP Address: %s\n", host);
+            }
+        }
+    }
+    freeifaddrs(ifaddr);
+}
+
+int main(int argc, char *argv[])
+{
+    /* MARK - New Test start, DB code Integration*/
+    printIP();
+    CLIENTDB Handle_Client[MAX_CLIENTS];
+    int opt = TRUE;
+    int master_socket, addrlen, new_socket, client_socket[MAX_CLIENTS], activity, i, valread, socket_fd;
+    int max_socket_fd;
+    struct sockaddr_in address;
+    char buffer[BUFFER_SIZE]; // data buffer of 1K
+
+    fd_set myreadset; // set of socket descriptors
+
+    char *message = "ECHO Daemon v1.0 \r\n"; // a message
+
+    // initialise all client_socket[] to 0 so not checked
+    for (i = 0; i < MAX_CLIENTS; i++)
+    {
+        client_socket[i] = 0;
+        CLIENTDB_ZERO(&Handle_Client[i]);
+    }
+
+    // create a master socket
+    if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("[-] MASTER SOCKET FAILED");
+        return EXIT_FAILURE;
+    }
+
+    // set master socket to allow multiple connections ,
+    // this is just a good habit, it will work without this
+    if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
+    {
+        perror("[-] setsockopt FAILED");
+        return EXIT_FAILURE;
+    }
+
+    // type of socket created
+    address.sin_family = AF_INET;         // IPV4
+    address.sin_addr.s_addr = INADDR_ANY; // anyip - local host
+    address.sin_port = htons(PORT);
+
+    // bind the socket to localhost port
+    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("[-] bind FAILED");
+        return EXIT_FAILURE;
+    }
+    printf("[*] Listener on port %d \n", PORT);
+
+    // try to specify maximum of 3 pending connections for the master socket
+    if (listen(master_socket, 3) < 0)
+    {
+        perror("[-] listen FAILED");
+        exit(EXIT_FAILURE);
+    }
+
+    // accept the incoming connection
+    addrlen = sizeof(address);
+    puts("[*] Waiting for connections ...");
+
+    while (TRUE)
+    {
+        // clear the socket set
+        FD_ZERO(&myreadset);
+
+        // add master socket to set
+        FD_SET(master_socket, &myreadset);
+        max_socket_fd = master_socket;
+
+        // add child sockets to set
+        for (i = 0; i < MAX_CLIENTS; i++)
+        {
+            // socket descriptor
+            socket_fd = client_socket[i];
+            /* here ig gotta assign values to DB too*/
+
+            // if valid socket descriptor then add to read list
+            if (socket_fd > 0)
+                FD_SET(socket_fd, &myreadset);
+
+            // highest file descriptor number, need it for the select function
+            if (socket_fd > max_socket_fd)
+                max_socket_fd = socket_fd;
+        }
+
+        // wait for an activity on one of the sockets , timeout is NULL ,
+        // so wait indefinitely
+        activity = select(max_socket_fd + 1, &myreadset, NULL, NULL, NULL);
+
+        if ((activity < 0) && (errno != EINTR))
+        {
+            printf("[-] select ERROR");
+            return EXIT_FAILURE;
+        }
+
+        // If something happened on the master socket ,
+        // then its an incoming connection
+        if (FD_ISSET(master_socket, &myreadset))
+        {
+            if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+            {
+                perror("[-] accept FAILED");
+                return EXIT_FAILURE;
+            }
+
+            // inform user of socket number - used in send and receive commands
+            printf("[*] New connection from,\n\t\tClient socket : %d\n\t\tIP : %s\n\t\tPORT : %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+            // send new connection greeting message
+            // if (send(new_socket, message, strlen(message), 0) != strlen(message))
+            // {
+            //     perror("[-] send FAILED");
+            // }
+
+            // puts("[*] Welcome message sent successfully");
+
+            // add new socket to array of sockets
+            for (i = 0; i < MAX_CLIENTS; i++)
+            {
+                // if position is empty
+                if (client_socket[i] == 0)
+                {
+                    CLIENTDB_ADD(&Handle_Client[i], "Dummy Name", inet_ntoa(address.sin_addr), ntohs(address.sin_port), 1);
+                    client_socket[i] = new_socket;
+                    printf("[*] Adding to list of sockets as Client : %d\n", i);
+                    break;
+                }
+            }
+        }
+
+        // else its some IO operation on some other socket
+        for (i = 0; i < MAX_CLIENTS; i++)
+        {
+            socket_fd = client_socket[i];
+
+            if (FD_ISSET(socket_fd, &myreadset))
+            {
+                int choice;
+                // Check if it was for closing , and also read the
+                // incoming message
+                // if ((valread = read(socket_fd, buffer, 1024)) == 0)
+                if ((valread = read(socket_fd, &choice, sizeof(int))) == 0)
+                {
+                    // Somebody disconnected , get his details and print
+                    getpeername(socket_fd, (struct sockaddr *)&address,
+                                (socklen_t *)&addrlen);
+                    printf("[*] Host disconnected,\n\t\tIP : %s\n\t\tPORT : %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+                    puts("[*] Waiting for connections ...");
+
+                    // here we can decremenet cleint entry ig - TD
+                    CLIENTDB_ZERO(&Handle_Client[i]);
+                    // Close the socket and mark as 0 in list for reuse
+                    close(socket_fd);
+                    client_socket[i] = 0;
+                }
+
+                // Echo back the message that came in
+                else
+                {
+                    char LIST[MAX_MESSAGE_LENGTH];
+                    printf("[*] List of Online Clients:\n");
+                    for (int i = 0; i < MAX_CLIENTS; i++)
+                    {
+                        if (Handle_Client[i].IP == NULL)
+                            continue;
+                        printf("\t-> Client: %d - IP/PORT : %s:%d\n", i, Handle_Client[i].IP, Handle_Client[i].port);
+                        // sprintf(LIST, "\t-> Client: %d - IP/PORT : %s:%d\n", i, Handle_Client[i].IP, Handle_Client[i].port);
+                    }
+                    if (choice == 7)
+                    {
+                        read(socket_fd, &(Handle_Client[i].client_EP.endpointPort), sizeof(int));
+                    }
+                    
+                    if (choice == 1)    // to send Online Users to Client
+                    {
+                        for (int i = 0; i < MAX_CLIENTS; i++)
+                        {
+                            if (Handle_Client[i].IP == NULL)
+                                continue;
+                            dprintf(socket_fd, "\t-> Client: %d -IP/PORT: %s:%d -ENDPOINT PORT %d\n", i, Handle_Client[i].IP, Handle_Client[i].port, Handle_Client[i].client_EP.endpointPort);
+                            // sprintf(LIST, "\t-> Client: %d - IP/PORT : %s:%d\n", i, Handle_Client[i].IP, Handle_Client[i].port);
+                        }
+                    }
+                    // else if (choice == 2)   // To recieve the request for client endpoint.
+                    {
+                        // int user_nmbr;
+                        // read(socket_fd, &user_nmbr, sizeof(int));
+                        // printf("[*] Client: %d requested for Client: %d endpoint.\n", i, user_nmbr);
+                        // if( i == user_nmbr)
+                        // {
+                        //     printf("[-] Invalid Self Call.\n");
+                        //     write(socket_fd, "-1", sizeof(int));
+                        // }
+                    }
+
+                    // set the string terminating NULL byte on the end
+                    // of the data read
+                    // buffer[valread] = '\0';
+                    // send(socket_fd, buffer, strlen(buffer), 0);
+
+                    /* Now sending one client port to the other. The first client will act as a server and
+                     the other will try to connect to that server using the port */
+                }
+            }
+        }
+    }
+
+    return 0;
+}
